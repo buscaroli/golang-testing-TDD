ede09d02fe5add090b825da09eaa345b8cf388e4
buscaroli <m.buscaroli@outlook.com>
2022-11-07 21:19:34 +0000

    update HelloUser() to allow user to select a spanish greeting

--- hello.go
+++ hello.go
@@ -4,19 +4,30 @@ import "fmt"
 
 // Constants should improve performance of your application as it saves you creating the "Hello, " string instance every time Hello is called.
 const HelloPrefix = "Hello, "
+const HolaPrefix = "Hola, "
+const spanish = "spanish"
 
 func Hello() string {
 	return "Hello, World!"
 }
 
-func HelloUser(u string) string {
+func HelloUser(u string, l string) string {
 	if u == "" {
+		if l == spanish {
+			return "Hola!"
+		}
 		return "Hello there!"
+
+	} else {
+		if l == spanish {
+			return HolaPrefix + u + "!"
+		}
 	}
+
 	return HelloPrefix + u + "!"
 }
 
 func main() {
 	fmt.Println(Hello())
-	fmt.Println(HelloUser("Matt"))
+	fmt.Println(HelloUser("Matt", ""))
 }
