7837d067a379d01a12ecc29e93d6a794d4522976
buscaroli <m.buscaroli@outlook.com>
2022-11-11 17:47:16 +0000

    add a structs package with tests

---
+++ structs/structs_test.go
@@ -0,0 +1,37 @@
+package structs
+
+import (
+	"math"
+	"testing"
+)
+
+func TestPerimeter(t *testing.T) {
+	t.Run("Calculates perimeter of the Rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 5.0}
+		result := rectangle.Perimeter()
+		expect := 30.0
+
+		CheckCorrectResult(t, expect, result)
+	})
+
+	t.Run("Calculates circumference of Circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		result := circle.Circumference()
+		expect := 2 * math.Pi * 10.0
+		CheckCorrectResult(t, result, expect)
+	})
+}
+
+func TestArea(t *testing.T) {
+	rectangle := Rectangle{10.0, 5.0}
+	result := rectangle.Area()
+	expect := 50.0
+
+	CheckCorrectResult(t, result, expect)
+}
+
+func CheckCorrectResult(t *testing.T, result, expect float64) {
+	if result != expect {
+		t.Errorf("\nresult: %f\nexpect: %f\n", result, expect)
+	}
+}
