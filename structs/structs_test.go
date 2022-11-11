9b1c69dcdf506316348caa24760a2953ec7fd078
buscaroli <m.buscaroli@outlook.com>
2022-11-11 18:50:14 +0000

    add the circle struct with some methods and test it

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -23,11 +23,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 5.0}
-	result := rectangle.Area()
-	expect := 50.0
+	t.Run("Calculates the area of a Rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 5.0}
+		result := rectangle.Area()
+		expect := 50.0
 
-	CheckCorrectResult(t, result, expect)
+		CheckCorrectResult(t, result, expect)
+	})
+
+	t.Run("Calculates the area of a circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		result := circle.Area()
+		expect := math.Pi * 100.00
+
+		CheckCorrectResult(t, result, expect)
+	})
 }
 
 func CheckCorrectResult(t *testing.T, result, expect float64) {
