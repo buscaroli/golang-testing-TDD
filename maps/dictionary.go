bfbe134e6fe373fe799d7ff450dd72fe88bac4e3
buscaroli <m.buscaroli@outlook.com>
2022-11-18 15:37:03 +0000

    refactor the Errors into a separate DictionaryError type

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,11 +1,17 @@
 package maps
 
-import "errors"
-
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("key does not exist")
-var ErrKeyExists = errors.New("key already in dictionary")
+type DictionaryError string
+
+const (
+	ErrNotFound  = DictionaryError("key does not exist")
+	ErrKeyExists = DictionaryError("key already in dictionary")
+)
+
+func (e DictionaryError) Error() string {
+	return string(e)
+}
 
 // Search if a given key exists in the dictionary and returns the value. If the key does not exists it returns an error.
 func (d Dictionary) Search(k string) (string, error) {
@@ -20,7 +26,7 @@ func (d Dictionary) Search(k string) (string, error) {
 // Adds a key to the dictionary. If the key already exists returns an error.
 func (d Dictionary) Add(k, v string) error {
 
-	result, _ := d[k]
+	result := d[k]
 	if result != "" {
 		return ErrKeyExists
 	}
