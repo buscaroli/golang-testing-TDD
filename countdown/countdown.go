1314c92ce3f826c20146d164b4e8bd874aacda5d
buscaroli <m.buscaroli@outlook.com>
2022-11-22 16:55:22 +0000

    mock the sleeping functionality of the Countdown method to avoid waiting for 3 secs everytime the test is run

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -10,14 +10,35 @@ import (
 const countdownStart = 3
 const finalWord = "Go!"
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+// Method that wait for 1 second.
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+// Method that increases the Calls counter, used to mock Sleep method of DefaultSleeper. Will be used instead of it for testing the Countdown function without waiting for 3 seconds everytime the test is run.
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
--- countdown/countdown_test.go
+++ countdown/countdown_test.go
@@ -7,13 +7,18 @@ import (
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	result := buffer.String()
 	expect := "3\n2\n1\nGo!"
 
 	if result != expect {
 		t.Errorf("\nresult: %q\nexpect: %q\n", result, expect)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("\nwrong number of calls.\ncounted: %q\nexpected: 3\n", spySleeper.Calls)
 	}
 }
