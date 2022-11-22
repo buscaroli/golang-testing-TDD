b839637180356d93040e2ce528b2a26a645d5a61
buscaroli <m.buscaroli@outlook.com>
2022-11-22 17:31:19 +0000

    test the order of the printed text and sleeps is correct

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -9,6 +9,8 @@ import (
 
 const countdownStart = 3
 const finalWord = "Go!"
+const sleep = "sleep"
+const write = "write"
 
 type Sleeper interface {
 	Sleep()
@@ -30,11 +32,38 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
+
+	fmt.Fprint(out, finalWord)
+}
+
+// CAREFUL! If used instead of Countdown, this function would still pass as the Sleep method has been called three times, however the functions misbehaves by sleeping for 3 consecutive seconds and then printing the countdown all in one go!
+func LogicallyWrongCountdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+	}
+
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+
 	fmt.Fprint(out, finalWord)
 }
 
--- countdown/countdown_test.go
+++ countdown/countdown_test.go
@@ -2,23 +2,42 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
+	t.Run("tests Sleep is called three times", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpySleeper{}
 
-	Countdown(buffer, spySleeper)
+		Countdown(buffer, spySleeper)
 
-	result := buffer.String()
-	expect := "3\n2\n1\nGo!"
+		result := buffer.String()
+		expect := "3\n2\n1\nGo!"
 
-	if result != expect {
-		t.Errorf("\nresult: %q\nexpect: %q\n", result, expect)
-	}
+		if result != expect {
+			t.Errorf("\nresult: %q\nexpect: %q\n", result, expect)
+		}
+	})
 
-	if spySleeper.Calls != 3 {
-		t.Errorf("\nwrong number of calls.\ncounted: %q\nexpected: 3\n", spySleeper.Calls)
-	}
+	t.Run("tests the order of sleeps and prints: first sleep, then print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		// Replace Countdown with LogicallyWrongCountdown to see the test failing
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		expect := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(expect, spySleepPrinter.Calls) {
+			t.Errorf("\nexpected: %v\ngot: %v\n", expect, spySleepPrinter.Calls)
+		}
+	})
 }
